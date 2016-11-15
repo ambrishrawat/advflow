5de87f9b6920d700b8ec3a9df2ede1efbba0b437
Ambrish Rawat Ambrish.Rawat@ie.ibm.com <ambrish@dccxl010.pok.ibm.com>
2016-11-15 15:49:39 -0500

    saving png for cifar from keras load

--- preprocessing/make_csv.py
+++ preprocessing/make_csv.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 from os import listdir
 import csv
 import os
@@ -12,6 +11,10 @@ import matplotlib
 import matplotlib.pyplot as plt
 import argparse
 from pathlib import Path
+import _pickle as cPickle
+from scipy.misc import imsave
+
+''' Class for preprocessing tinyImageNet dataset '''
 
 class tinyImageNet(object):
 
@@ -77,8 +80,10 @@ class tinyImageNet(object):
         self.make_train_csv()
         self.make_val_csv()
 
-import _pickle as cPickle
-from scipy.misc import imsave
+
+
+''' Global Variables, utility functions and class defintion for CIFAR raw  dataset '''
+
 # Width and height of each image.
 img_size = 32
 
@@ -153,8 +158,11 @@ class cifar10(object):
     
     
     def __init__(self,source_path = None,dest_path=None):
+
+        #fpath is used to maintainthe destiation path where the csvfiles will be stored
         self.fpath = dest_path
-        if fpath is None:
+
+        if self.fpath is None:
             raise ValueError
         self.classes = 10
         #self.save_jpegs(source_path=source_path)
@@ -173,6 +181,7 @@ class cifar10(object):
         #train_batches = batches[0:5]
         #test_batches = batches[5:6]
         
+        # (crude) sanity check to see if all the batch files are present or not
         from functools import reduce
         test = reduce(lambda x,y : x and y, [b in files for b in batches])
         if test is False:
@@ -277,18 +286,206 @@ class cifar10(object):
 
 
 
+''' Class defintion for preprocessing keras loaded CIFAR data set'''
+
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
+from keras.utils import np_utils
+from keras.models import model_from_json
+
+batch_size = 32
+nb_classes = 10
+nb_epoch = 200
+
+# input image dimensions
+img_rows, img_cols = 32, 32
+# the CIFAR10 images are RGB
+img_channels = 3
+
+
+def save_figs2(dest_path=None, mode=None, cls=None, filenames=None, images=None):
+    '''helper function to save images'''
+    if mode=='train':
+        for i in range(filenames.shape[0]):
+            imsave(os.path.join(dest_path,mode,classes[cls[i]],filenames[i]),images[i])
+    else:
+        for i in range(filenames.shape[0]):
+            imsave(os.path.join(dest_path,mode,'images',filenames[i]),images[i])
+            with open(os.path.join(dest_path,mode,'annotations.csv'), 'a') as csvfile:
+                csvwriter = csv.writer(csvfile, delimiter=',')
+                csvwriter.writerow([filenames[i],classes[cls[i]]]) #csv header
+            # save this in annotations classes[cls[i]]
+
+
+    pass
+
+
+class cifar10_keras(object):
+    '''
+    Preprocessing the data set and making AdvFlow compatible csv files for CIFAR10
+    Also, saves the JPEGs for the trianing and test set which facilitates
+    easy visualisation of adversarial images
+    '''
+    
+    
+    def __init__(self, dest_path = None):
+        
+        
+        #fpath is used to maintainthe destiation path where the csvfiles will be stored
+        self.fpath = dest_path
+
+        if self.fpath is None:
+            raise ValueError
+
+        # the data, shuffled and split between train and test sets
+        (self.X_train, self.y_train), (self.X_test, self.y_test) = cifar10.load_data()
+        print('X_train shape:', self.X_train.shape)
+        print(self.y_train.shape, 'train samples')
+        print(self.y_test.shape, 'test samples')
+
+        # convert class vectors to binary class matrices
+        self.Y_train = np_utils.to_categorical(self.y_train, nb_classes)
+        self.Y_test = np_utils.to_categorical(self.y_test, nb_classes)
+
+        self.X_train = self.X_train.astype('float32')
+        self.X_test = self.X_test.astype('float32')
+        self.X_train /= 255
+        self.X_test /= 255
+
+
+        self.classes = 10
+        #self.save_jpegs()
+    
+    def save_jpegs(self,source_path=None):
+        
+        ''' saves JPEGs structured into class wise sub folders'''
+        
+        dest_path = self.fpath
+             
+        #make class dirs
+        if not(os.path.exists(dest_path)):
+            os.makedirs(dest_path)
+        
+        for c in classes:
+            if not(os.path.exists(os.path.join(dest_path,'train',c))):
+                os.makedirs(os.path.join(dest_path,'train',c))
+        if not(os.path.exists(os.path.join(dest_path,'test','images'))):
+            os.makedirs(os.path.join(dest_path,'test','images'))
+        
+
+        # Get the raw images.
+        raw_images = self.X_train
+    
+        # Get the class-numbers for each image. Convert to numpy-array.
+        cls = self.y_train
+
+        #filenames
+        filenames = np.arange(self.X_train.shape[0])
+        filenames = np.array(list(map(lambda x: str(x) + '.png',filenames)))
+        # Convert the images.
+        #images = _convert_images(raw_images)
+        #print(images.shape)
+        
+        # save images as jpegs
+        save_figs2(dest_path=dest_path, mode='train',cls=cls, filenames=filenames, images=self.X_train)
+        
+
+    
+        # Get the raw images.
+        raw_images = self.X_test
+    
+        # Get the class-numbers for each image. Convert to numpy-array.
+        cls = self.y_test
+
+        #filenames
+        filenames = np.arange(self.X_test.shape[0])
+        filenames = np.array(list(map(lambda x: str(x) + '.png',filenames)))
+        # Convert the images.
+        #images = _convert_images(raw_images)
+        #print(images.shape)
+        
+        # save images as jpegs
+        save_figs2(dest_path=dest_path, mode='test',cls=cls, filenames=filenames, images=self.X_test)
+
+    def make_train_csv(self, traincsv=None):
+        '''
+        for cifar10_sets folder structure (train)
+        '''
+        self.classes = classes
+        num_classes = len(self.classes)
+        
+        traincsv_file = Path(traincsv)
+        if False:
+            pass
+        else:
+            with open(traincsv, 'wt') as csvfile:
+                csvwriter = csv.writer(csvfile, delimiter=',')
+                csvwriter.writerow(['filename']+self.classes) #csv header
+    
+                c_idx = 0
+                for class_ in self.classes:
+                    class_path = os.path.join(self.fpath,'train',class_)
+                    images_ = listdir(class_path)
+                    for image_ in images_:
+    
+                        file_path = os.path.join(class_path,image_)
+    
+                        label = np.zeros(num_classes)
+                        label[c_idx] = 1
+    
+                        csvwriter.writerow([file_path] + list(map(str, label)))
+                    c_idx+=1
+
+    def make_test_csv(self, valcsv=None):
+        '''
+        for tiny ImageNet folder structure (val)
+        '''
+        valcsv_file = Path(valcsv)
+        if valcsv_file.is_file():
+            pass
+        else:
+            with open(os.path.join(self.fpath,'test','annotations.csv'), 'rt') as csvfile:
+                
+                reader = csv.reader(csvfile, delimiter=',')
+                
+                with open(valcsv, 'wt') as csvfile:
+                    
+                    csvwriter = csv.writer(csvfile, delimiter=',')
+                    csvwriter.writerow(['filename']+self.classes)
+                    
+                    for row in reader:
+
+                        file_path = os.path.join(self.fpath,'test','images',row[0])
+                        
+                        label = np.zeros(len(self.classes))
+                        label[self.classes.index(row[1])] = 1
+                        
+                        csvwriter.writerow([file_path] + list(map(str, label)))            
+                
+    def make_csvs(self):
+        self.make_train_csv(traincsv='train_cifar10_keras.csv')
+        self.make_test_csv(valcsv='test_cifar10_keras.csv')
+
+
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Make csv files for train, val and test sets of tinyImageNet')
     #parser.add_argument('--fpath', type=str, default='/dccstor/dlw/ambrish/data/tinyImageNet/tiny-imagenet-200/', help='location of the downloaded dataset (cifar10 or tinyImageNet)')
     parser.add_argument('--fpath', type=str, default='/dccstor/dlw/ambrish/data/cifar10/cifar-10-batches-py/', help='location of the downloaded dataset (cifar10 or tinyImageNet)')
-    parser.add_argument('--cifarpath', type=str, default='/dccstor/dlw/ambrish/data/cifar10/cifar10-sets/', help='location of the save cifar jpegs')
+    parser.add_argument('--cifarpath', type=str, default='/dccstor/dlw/ambrish/data/cifar10/cifar10-keras-sets/', help='location of the saved cifar jpegs')
     args = parser.parse_args()
 
     fpath = args.fpath
     cifarpath = args.cifarpath
 
     #tset = tinyImageNet(fpath)
-    tset = cifar10(source_path=fpath,dest_path=cifarpath)
+    #tset = cifar10(source_path=fpath,dest_path=cifarpath)
+    tset = cifar10_keras(dest_path=cifarpath)
 
     tset.make_csvs()
